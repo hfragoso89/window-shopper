29cf78b3fa54174fa324c50b3c4c788e117b2c8f
Fragoso <hfragoso@MXN1800100-2.local>
2020-02-05 22:28:06 -0600

    first commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Fragoso, Hector on 2/5/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
